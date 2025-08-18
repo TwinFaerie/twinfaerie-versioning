f7641112d35fb5516c35fdf6fd206c85f4cab4d6
BlueSlime <umbelbiru@twinfaerie.com>
2025-08-18 14:24:05 +0700

    ad keystore setup

--- Editor/VersioningCI.cs
+++ Editor/VersioningCI.cs
@@ -7,15 +7,26 @@ namespace TF.Versioning.Editor
     {
         public static void SetVersion()
         {
-            // Set version
-            string version = Environment.GetEnvironmentVariable("CI_TAG") ?? "0.0.1";
+            var version = Environment.GetEnvironmentVariable("CI_TAG") ?? "0.0.1";
             PlayerSettings.bundleVersion = version;
 
-            string buildIdStr = Environment.GetEnvironmentVariable("CI_BUILD_ID") ?? "1";
+            var buildIdStr = Environment.GetEnvironmentVariable("CI_BUILD_ID") ?? "1";
             if (int.TryParse(buildIdStr, out int buildId)) PlayerSettings.Android.bundleVersionCode = buildId;
             
             Console.WriteLine($"[TF.VersioningCI] Applied Version: {PlayerSettings.bundleVersion}");
             Console.WriteLine($"[TF.VersioningCI] Applied Android BuildCode: {PlayerSettings.Android.bundleVersionCode}");
+        }
+
+        public static void SetAndroidKeystore()
+        {
+            PlayerSettings.Android.useCustomKeystore = true;
+            PlayerSettings.Android.keystoreName = Environment.GetEnvironmentVariable("CI_KEYSTORE_PATH");
+            PlayerSettings.Android.keystorePass = Environment.GetEnvironmentVariable("CI_KEYSTORE_PASS");
+            PlayerSettings.Android.keyaliasName = Environment.GetEnvironmentVariable("CI_ALIAS_NAME");
+            PlayerSettings.Android.keyaliasPass = Environment.GetEnvironmentVariable("CI_ALIAS_PASS");
+            
+            Console.WriteLine($"[TF.VersioningCI] Using Keystore at path: {PlayerSettings.Android.keystoreName}");
+            Console.WriteLine($"[TF.VersioningCI] Using Keystore alias: {PlayerSettings.Android.keyaliasName}");
         }
     }
 }
